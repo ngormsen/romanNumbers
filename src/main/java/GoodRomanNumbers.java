c74cdcbbffe40dcd819fd342b135f9993317fffb
ngormsen <50167165+ngormsen@users.noreply.github.com>
2021-07-12 10:00:57 +0200

    Add more refactoring

--- src/main/java/GoodRomanNumbers.java
+++ src/main/java/GoodRomanNumbers.java
@@ -1,57 +1,75 @@
+enum ROMAN_NUMBER {
+    M(1000),
+    CM(900),
+    D(500),
+    CD(400),
+    C(100),
+    XC(90),
+    L(50),
+    XL(40),
+    X(10),
+    IX(9),
+    V(5),
+    IV(4),
+    I(1);
+
+    int arabic;
+
+    ROMAN_NUMBER(int arabic){
+        this.arabic = arabic;
+    }
+
+    public static ROMAN_NUMBER[] getRomanNumbers(){
+        return ROMAN_NUMBER.values();
+    }
+}
+
+
 public class GoodRomanNumbers {
+    ROMAN_NUMBER[] romanNumbers = ROMAN_NUMBER.getRomanNumbers();
 
-    private final String[] romanNumbers = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    private final int[] arabicNumbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-
-    public String parseRomanNumbersToArabicNumbersOrArabicNumbersToRomanNumbers(String inputNumber) {
-        if (isValidRomanNumber(inputNumber)) {
-            return romanToArabic(inputNumber);
-        } else if (isValidArabicNumber(inputNumber)) {
-            return arabicToRoman(inputNumber);
-        } else {
-            return "Wrong input!";
-        }
+    public String parse(String inputNumber) {
+        if (isArabic(inputNumber)) return romanNumberOf(inputNumber);
+        else if (isRoman(inputNumber)) return arabicNumberOf(inputNumber);
+        else return "Wrong input!";
     }
 
-    private boolean isValidArabicNumber(String inputNumber) {
-        return inputNumber.matches("[0-9]+") && !inputNumber.matches("0+");
-    }
+    private String romanNumberOf(String inputNumber) {
+        int arabicNumber = Integer.parseInt(inputNumber);
 
-    private boolean isValidRomanNumber(String inputNumber) {
-        return inputNumber.matches("[A-Z]+");
-    }
-
-    private String romanToArabic(String romanNumber) {
-        int resultingArabicNumber = 0;
-        int idx = 0;
-        while (romanNumber.length() > 0 && idx < romanNumbers.length) {
-            if (romanNumber.startsWith(romanNumbers[idx])) {
-                resultingArabicNumber += arabicNumbers[idx];
-                romanNumber = romanNumber.substring(romanNumbers[idx].length());
-            } else {
-                idx++;
+        StringBuilder resultingRomanNumber = new StringBuilder();
+        for(ROMAN_NUMBER romanNumber : romanNumbers){
+            while (arabicNumber >= romanNumber.arabic){
+                resultingRomanNumber.append(romanNumber);
+                arabicNumber -= romanNumber.arabic;
             }
         }
+
+        return resultingRomanNumber.toString();
+    }
+
+    private String arabicNumberOf(String inputNumber) {
+        int resultingArabicNumber = 0;
+        for(ROMAN_NUMBER romanNumber : romanNumbers){
+            while(inputNumber.length() > 0 && inputNumber.startsWith(romanNumber.toString())){
+                resultingArabicNumber += romanNumber.arabic;
+                inputNumber = inputNumber.substring(romanNumber.toString().length());
+            }
+        }
+
         return String.valueOf(resultingArabicNumber);
     }
 
-    private String arabicToRoman(String inputNumber) {
-        int n = Integer.parseInt(inputNumber);
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < romanNumbers.length; i++) {
-            while (n >= arabicNumbers[i]) {
-                result.append(romanNumbers[i]);
-                n -= arabicNumbers[i];
-            }
-        }
-        return result.toString();
+    private boolean isArabic(String number) {
+        return number.matches("[0-9]+") && !number.matches("0+");
     }
 
+    private boolean isRoman(String number) {
+        return number.matches("[a-zA-Z]+");
+    }
 
     public static void main(String[] args) {
-        GoodRomanNumbers goodRomanNumbers = new GoodRomanNumbers();
-        System.out.println(goodRomanNumbers.parseRomanNumbersToArabicNumbersOrArabicNumbersToRomanNumbers(args[0]));
+        GoodRomanNumbers romanNumbers = new GoodRomanNumbers();
+        System.out.println(romanNumbers.parse(args[0]));
     }
-
 }
